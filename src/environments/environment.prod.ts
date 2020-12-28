1f1efec00d040d64a1ec2d088c439dfdb625c7c9
Julijana Ugrinov <jugrinov@gmail.com>
2020-12-28 20:05:33 +0100

    Firebase settings changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDr33FDLHabJTV35OZdzB7FWRnhRFNMusc',
-    authDomain: 'code-snippet-app.firebaseapp.com',
-    databaseURL: 'https://code-snippet-app.firebaseio.com',
-    projectId: 'code-snippet-app',
-    storageBucket: 'code-snippet-app.appspot.com',
-    messagingSenderId: '776786388085'
+    apiKey: "AIzaSyCN4drA1NHYu8L2LptS5pkl2CWsq43e8Rc",
+    authDomain: "snippetsapp-5cad5.firebaseapp.com",
+    databaseURL: "https://snippetsapp-5cad5-default-rtdb.firebaseio.com",
+    projectId: "snippetsapp-5cad5",
+    storageBucket: "snippetsapp-5cad5.appspot.com",
+    messagingSenderId: "1075498389583",
+    appId: "1:1075498389583:web:b491b0dc59b0d3430d9a3a",
+    measurementId: "G-S8M0F02GB6"
   }
 };
